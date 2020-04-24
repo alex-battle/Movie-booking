84827e0154db979d739496bd233cc50ebbe441b9
Alex Battle <58659536+alex-battle@users.noreply.github.com>
2020-04-24 16:46:44 -0400

    click events updating total and count

--- script.js
+++ script.js
@@ -7,8 +7,21 @@ const movieSelect = document.getElementById('movie')
 
 const ticketPrice = +movieSelect.value
 
+// Update total and count
+function updateSelectedCount(){
+    const selectedSeats = document.querySelectorAll('.row .seat.selected')
+    // console.log(selectedSeats)
+    const selectedSeatsCount = selectedSeats.length
+    // console.log(selectedSeatsCount)
+    count.innerText = selectedSeatsCount
+    total.innerText = selectedSeatsCount * ticketPrice
+}
+
 container.addEventListener('click', e => {
-    if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')){
-        console.log('hello')
+    if(e.target.classList.contains('seat') && 
+    !e.target.classList.contains('occupied')){
+        e.target.classList.toggle('selected')
+
+        updateSelectedCount()
     }
 })
