3cfceaca3879d3dd1491fdcf1921b31be171a978
Alex Battle <58659536+alex-battle@users.noreply.github.com>
2020-04-24 17:42:09 -0400

    populate UI with Saved Data

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const count = document.getElementById('count')
 const total = document.getElementById('total')
 const movieSelect = document.getElementById('movie')
 
+populateUI()
+
 let ticketPrice = +movieSelect.value
 
 // save selected movie index and price
@@ -30,6 +32,23 @@ function updateSelectedCount(){
     count.innerText = selectedSeatsCount
     total.innerText = selectedSeatsCount * ticketPrice
 }
+
+// Get data from local storage and populate UI
+
+function populateUI(){
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'))
+    if(selectedSeats !== null && selectedSeats.length > 0){
+        seats.forEach((seat, index) => {
+            if(selectedSeats.indexOf(index) > -1){
+                seat.classList.add('selected')
+            }
+        })
+    }
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex')
+    if(selectedMovieIndex !== null){
+        movieSelect.selectedIndex = selectedMovieIndex
+    }
+}
 // Movie Select event
 movieSelect.addEventListener('change', e =>{
     ticketPrice = +e.target.value
@@ -45,3 +64,6 @@ container.addEventListener('click', e => {
         updateSelectedCount()
     }
 })
+
+// initial count and total set
+updateSelectedCount()
