a276bea1d815eb7819143825ae2aa251fbf72e2e
Alex Battle <58659536+alex-battle@users.noreply.github.com>
2020-04-24 17:16:01 -0400

    create a local storage

--- script.js
+++ script.js
@@ -7,10 +7,24 @@ const movieSelect = document.getElementById('movie')
 
 let ticketPrice = +movieSelect.value
 
+// save selected movie index and price
+function setMovieData(movieIndex, moviePrice){
+    localStorage.setItem('selectedMovieIndex', movieIndex)
+    localStorage.setItem('selectedMoviePrice', moviePrice)
+}
 // Update total and count
 function updateSelectedCount(){
     const selectedSeats = document.querySelectorAll('.row .seat.selected')
     // console.log(selectedSeats)
+
+    // Copy selected seats into array
+    // Map through array
+    // return a new array of indexes
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat))
+    // console.log(seatsIndex)
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex))
+
     const selectedSeatsCount = selectedSeats.length
     // console.log(selectedSeatsCount)
     count.innerText = selectedSeatsCount
@@ -19,6 +33,7 @@ function updateSelectedCount(){
 // Movie Select event
 movieSelect.addEventListener('change', e =>{
     ticketPrice = +e.target.value
+    setMovieData(e.target.selectedIndex, e.target.value)
     updateSelectedCount()
 })
 // Seat click event
